dd87e780743f07d3aec192b9bff7f5d834d6bf8b
Shreyas Ashtamkar <shreyu@programmer.net>
2021-05-08 17:38:07 +0530

    "Automated URL Negotiation"

--- webserver.py
+++ webserver.py
@@ -21,73 +21,73 @@ detected_objects = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # if v_url:
-    #     print(v_url)
-    #     return render_template('index.html', vurl = v_url)
-    # else:
-    return render_template('index.html', vurl = v_url)
+	# if v_url:
+	#     print(v_url)
+	#     return render_template('index.html', vurl = v_url)
+	# else:
+	return render_template('index.html', vurl = v_url)
 
 @app.route('/setvurl', methods=['POST'])
 def seturl():
-    global v_url
-    print(request.form.get('vurl'))
-    v_url = request.form.get('vurl')
-    return ""
+	global v_url
+	print(request.form.get('vurl'))
+	v_url = request.form.get('vurl')
+	return ""
 
 @app.route('/unsetvurl', methods=['POST'])
 def unseturl():
-    global v_url
-    v_url = None
-    print("url unset")
-    return "True"
+	global v_url
+	v_url = None
+	print("url unset")
+	return "True"
 
 @app.route('/getvurl', methods=['GET', 'POST'])
 def getvurl():
-    return v_url
+	return v_url
 
 @app.route('/isready', methods=['GET', 'POST'])
 def ready():
-    return "True"
+	return "True"
 
 @app.route('/setobjects', methods=['POST'])
 def setObjects():
-    global detected_objects
-    detected_objects = fromJSON(detected_objects)
+	global detected_objects
+	detected_objects = fromJSON(detected_objects)
 
 @app.route('/getobjects', methods=['GET', 'POST'])
 def getObjects():
-    detected_objects = ["demo1", "demo2", "demo3"]
+	detected_objects = ["demo1", "demo2", "demo3"]
 
-    if v_url:
-        detected_objects = []
-	    for _ in range(2):
-	        many_boxes = detect_custom(
-	            video_path          = v_url        ,
-	            Yolo                = yolo         ,
-	            output_path         = ''           , 
-	            show                = False        ,
-	            score_threshold     = 0.4          ,
-	            rectangle_colors    = (255,0,0)
-	        )
+	if v_url:
+		detected_objects = []
+		for _ in range(2):
+			many_boxes = detect_custom(
+				video_path          = v_url        ,
+				Yolo                = yolo         ,
+				output_path         = ''           , 
+				show                = False        ,
+				score_threshold     = 0.4          ,
+				rectangle_colors    = (255,0,0)
+			)
 
-	        for single_box in many_boxes:
-	            if single_box not in detected_objects:
-	                detected_objects.append(single_box)
+			for single_box in many_boxes:
+				if single_box not in detected_objects:
+					detected_objects.append(single_box)
 
-    if request.method=='GET':
-        detected_objects = render_template('output.html', vurl = str(v_url), data= detected_objects)
-    else:
-        detected_objects = toJSON(detected_objects)
+	if request.method=='GET':
+		detected_objects = render_template('output.html', vurl = str(v_url), data= detected_objects)
+	else:
+		detected_objects = toJSON(detected_objects)
 
-    return detected_objects
+	return detected_objects
 
 
 if __name__=='__main__':
-    from pyngrok import ngrok
-    myurl = ngrok.connect(8000)
-    
-    post(negotiator_url+"/seturl", data={"url":myurl.public_url})
-    
-    app.run(port=8000)
+	from pyngrok import ngrok
+	myurl = ngrok.connect(8000)
+	
+	post(negotiator_url+"/seturl", data={"url":myurl.public_url})
+	
+	app.run(port=8000)
 
-    
+	
